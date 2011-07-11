a0c612a521ca2e327ba4d7375ec4c9bd1fcc2057
= <=>
2011-07-11 11:08:04 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomade::Application.config.secret_token = 'e40cfe71a934e55f5ee467578625cb214143178c7cd83972ca48f4533636ced5f8525f1e5addf9be2ffde418e975d4062a17d34980e4c170276e89fc78d4fcfb'
